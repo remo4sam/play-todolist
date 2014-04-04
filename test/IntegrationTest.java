397b5a8173f06f71becc127f6377bb236ccf7a65
Timothy Asiimwe <atimothee@gmail.com>
2014-04-04 14:45:30 +0300

    added bootstrap styling and modified test for the listOfNames page

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -34,7 +34,7 @@ public class IntegrationTest {
         running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
             public void invoke(TestBrowser browser) {
                 browser.goTo("http://localhost:3333/tasks");
-                assertThat(browser.pageSource()).contains("task(s)");
+                assertThat(browser.pageSource()).contains("Add a New Task");
 
             }
         });
